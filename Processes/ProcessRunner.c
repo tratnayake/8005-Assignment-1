86f3cf6827b106ee9a0435f05fd76c4c2ce010af
Thilina Ratnayake <thilinaratnayake@Thilinas-MacBook-Air.local>
2015-01-19 19:13:12 -0800

    First Commit from laptop

---
+++ Processes/ProcessRunner.c
@@ -0,0 +1,179 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <openssl/sha.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int task(int fileNumber, long processNumber, long iterations, FILE *pFile);
+
+int main (int argc, char *argv[]) 
+{
+    pid_t childpid = 0; 
+    int i, n;
+
+	int status;
+  	pid_t pid;
+
+    //Creating space for vars that will be used later to dicate fileNumber and Process Number in outputfiles.
+    //char fileNumber[50];
+    
+
+    //For dateTime
+    time_t curtime1;
+    time(&curtime1);
+
+    FILE *pFile;
+
+    pFile = fopen("ProcessLogFile.txt","a");
+
+    printf("**PROCESSES PROGRAM START TIME: %s \n", ctime(&curtime1));
+    fprintf(pFile,"**PROCESSES PROGRAM START TIME: %s \n", ctime(&curtime1));
+    fflush(pFile);
+    
+
+    if (argc != 2)
+      {   /* check for valid number of command-line arguments */ 
+            fprintf(stderr, "Usage: %s processes\n", argv[0]);
+            return 1; 
+    }     
+
+    n = atoi(argv[1]);  
+
+    for (i = 0; i < n; i++)
+      if ((childpid = fork()) <= 0)
+      break;
+    
+      //printf("ChildPID: %ld \n",(long)childpid);
+      switch((long)childpid)
+      {
+        case -1:
+          perror("ERROR!\n");
+          break;
+
+        case 0:
+          //printf("I'm a child!\n");
+          //fprintf(stderr, "**CHILD** i:%d  process ID:%ld  parent ID:%ld  child ID:%ld\n",
+           //i, (long)getpid(), (long)getppid(), (long)childpid);
+
+          task(i,(long)getpid(),10000000,pFile);
+
+          break;
+
+        default:
+          //printf("I'm not a child?\n");
+          //printf("**PARENT**: i:%d  process ID:%ld  parent ID:%ld  child ID:%ld\n",
+           //i, (long)getpid(), (long)getppid(), (long)childpid);
+
+          
+          while (n > 0) {
+            pid = wait(&status);
+            printf("Child with PID %ld exited with status 0x%x.\n", (long)pid, status);
+            --n;  // TODO(pts): Remove pid from the pids array.
+          }
+
+          time_t curtime2;
+
+          time(&curtime2);
+          
+          //printf("PROGRAM END TIME: %s \n", ctime(&curtime2));
+          fprintf(pFile,"**PROGRAM END TIME**: %s \n", ctime(&curtime2));
+          printf("**PROGRAM END TIME**: %s \n", ctime(&curtime2));
+
+              break;
+          }
+
+    return 0; 
+}
+
+int task(int fileNumber, long processNumber, long iterations, FILE *pFile){
+   FILE *fp;
+
+
+    //Vars taken from Program Start
+    int fileNum = fileNumber;
+    int processID = processNumber;
+    long i,n;
+    int x;
+    n = iterations;
+
+
+    //Vars required for the fileName and Looping
+    char fileNumString[sizeof(fileNum)];
+    char loopCounterString[100];
+
+  
+
+    time_t processStartTime;
+    time_t processEndTime;
+    time_t iterationStartTime;
+    time_t iterationEndTime;
+
+
+
+    char outputFileName[1024];
+    strcpy(outputFileName, "./ProcessFiles/ProcessTaskOutputFile");
+    //Convert fileNum INT into a String
+          //DEBUG: printf("Before convert,fileNumString: %s",fileNumString);
+    sprintf(fileNumString,"%d",fileNum);
+          //DEBUG: printf("After convert,fileNumString: %s",fileNumString);
+    strcat(outputFileName, fileNumString);
+    strcat(outputFileName,".txt");
+
+    char data[1024];    
+    char hash[SHA_DIGEST_LENGTH];
+
+    //DEBUG: printf("Before print, the fileOutputName is: %s",outputFileName);
+
+    fp = fopen(outputFileName, "w");// "w" means that we are going to write on this file
+
+    processStartTime = time(NULL);
+    printf("** PROCESS: %d START** TIME %s \n\n",processID,asctime(localtime(&processStartTime)));
+    fprintf(fp, "PROCESS START: processID: %d TIME: %s \n", processID, asctime(localtime(&processStartTime)));
+    fprintf(pFile, "PROCESS START: processID: %d TIME: %s \n", processID, asctime(localtime(&processStartTime)));
+
+    //RUN FUNCTION HERE
+    for (i = 1; i <= n; i++)
+    {
+      //Convert i to a String
+      sprintf(loopCounterString,"%ld", i);
+      strcpy(data, "HASHSTRING");
+      //Add value of i  (concatenate) to END of string
+      strcat(data,loopCounterString);
+      size_t length = sizeof(data);
+      //Print string+i
+      //printf("Thing to hash is %s",data);
+      iterationStartTime = time(NULL);
+      fprintf(fp, "ITERATION %ld | TIME: %s \n", i, asctime (localtime(&iterationStartTime)) );
+     
+      
+      SHA(data,length,hash);
+
+
+
+      //printf("The hash is %x \n",hash);
+      fprintf(fp,"PLAINTEXT: %s\n",data);
+      fprintf(fp,"HASH:\n");
+
+      for(x = 0; x < sizeof(hash); x++ )
+      {
+     
+        fprintf(fp,"%x", (char)hash[x]);
+      }
+      
+      fprintf(fp,"\n");
+      
+      	iterationEndTime = time(NULL);
+       fprintf(fp, "END ITERATION %ld INSIDE PROCESS TIME: %s \n", i, asctime (localtime(&iterationEndTime)));
+     
+    }
+
+	processEndTime = time(NULL);
+    printf("** PROCESS: %d END** TIME %s \n\n",processID,asctime(localtime(&processEndTime)));
+    fprintf(fp, "PROCESS END: processID: %d TIME: %s \n", processID, asctime(localtime(&processEndTime)));
+     fprintf(pFile, "PROCESS END: processID: %d TIME: %s \n", processID, asctime(localtime(&processEndTime)));
+     
+    return 0; 
+}
---
+++ Threads/ThreadRunner.c
@@ -0,0 +1,189 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <openssl/sha.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <pthread.h>
+
+// look up the sched_yield() system call
+
+ struct Foo
+
+            {
+
+                        int x;
+
+                        int array[100];
+
+            };  /* note semi-colon here */
+
+
+/*----------- Function Prototypes ------------------*/
+void* task (void*);
+
+/*------------ Mutex Variables ---------------------*/
+pthread_mutex_t printfLock = PTHREAD_MUTEX_INITIALIZER;
+
+int main (void)
+{
+
+  FILE *pFile;
+
+  pFile = fopen("./ThreadLogFile.txt","w");
+
+  time_t programStartTime;
+  time_t programEndTime;
+
+  programStartTime = time(NULL);
+
+  printf("** THREADS PROGRAM START** TIME: %s  \n\n",asctime( localtime(&programStartTime)));
+  fprintf(pFile,"** THREADS PROGRAM START** TIME: %s  \n\n",asctime( localtime(&programStartTime)));
+  fflush(pFile);
+  fclose(pFile);
+
+  pthread_t thread1, thread2,thread3,thread4,thread5;
+  const char* msg1 = "Thread1";
+  const char* msg2 = "Thread2";
+  const char* msg3 = "Thread3";
+  const char* msg4 = "Thread4";
+  const char* msg5 = "Thread5";
+
+
+  pthread_create (&thread1, NULL, task, (void*)msg1);
+  pthread_create (&thread2, NULL, task, (void*)msg2);
+  pthread_create (&thread3, NULL, task, (void*)msg3);
+  pthread_create (&thread4, NULL, task, (void*)msg4);
+  pthread_create (&thread5, NULL, task, (void*)msg5);
+
+  pthread_join (thread1, NULL);
+  pthread_join (thread2, NULL);
+  pthread_join (thread3, NULL);
+  pthread_join (thread4, NULL);
+  pthread_join (thread5, NULL);
+
+  //printf ("Threads Complete\n");
+
+  programEndTime = time(NULL);
+
+  pFile = fopen("./ThreadLogFile.txt","a");
+  printf("** THREADS PROGRAM END** TIME: %s  \n\n",asctime( localtime(&programEndTime)));
+  fprintf(pFile,"** THREADS PROGRAM END** TIME: %s  \n\n",asctime( localtime(&programEndTime)));
+  
+
+  return 0;
+}
+
+/*----------  Output Message Function -------------*/
+void* task (void* msg)
+{
+
+    sched_yield();
+
+    pthread_mutex_lock (&printfLock);
+
+
+
+
+   int i,x;
+
+    //DEBUG: printf("Before print, the fileOutputName is: %s",outputFileName);
+
+    //fp = fopen(outputFileName, "w");// "w" means that we are going to write on this file
+
+    //fprintf(fp, "PROCESS START:  %d TIME: %s \n",  asctime (timeinfo));
+     //fprintf(pFile, "PROCESS START:  %d TIME: %s \n",  asctime (timeinfo));
+
+    //RUN FUNCTION HERE
+   
+   FILE *fp;
+   FILE *pFile;
+
+   pFile = fopen("ThreadLogFile.txt","a");
+
+   //Vars required to get currentDateTime
+   time_t threadTime;
+   time_t threadEndTime;
+   threadTime = time(NULL);
+
+   printf("** %s START** TIME: %s  \n\n",(char*) msg,asctime( localtime(&threadTime)));
+   fprintf(pFile," %s START TIME: %s  \n\n",(char*) msg,asctime( localtime(&threadTime)));
+   fflush(pFile);
+
+   char fileOutputString[100];
+   strcpy(fileOutputString,"./ThreadFiles/ThreadOutputFile");
+   strcat(fileOutputString,(char *) msg);
+   strcat(fileOutputString,".txt");
+
+   //printf("Output file is %s",fileOutputString);
+   //fileOutputString = "./TheadFiles/ThreadOutputFile";
+
+   fp = fopen(fileOutputString,"a");
+
+    for (i = 1; i <= 10000000; i++)
+    {
+      //printf("ITERATION %i START \n",i);
+      //Convert i to a String
+      //sprintf(loopCounterString,"%ld", i);
+      unsigned char data[1024];    
+      unsigned char hash[SHA_DIGEST_LENGTH];
+      char loopCounterString[100];
+
+      sprintf(loopCounterString,"%d",i);
+      strcpy(data, "HASHSTRING");
+      //Add value of i  (concatenate) to END of string
+      strcat(data,loopCounterString);
+      size_t length = sizeof(data);
+      //Print string+i
+      //printf("Thing to hash is %s",data);
+      //fprintf(fp, "ITERATION %ld, TIME: %s \n", i,  asctime (timeinfo) );
+      //strcat(fileOutputString, "./TheadFiles/ThreadOutputFile");
+      //strcat(fileOutputString,loopCounterString);
+      //strcat(fileOutputString,".txt");
+      //printf("FileOutputSTring is %s\n", fileOutputString);
+
+     //printf("Thing to hash is %s\n",data); 
+      SHA(data,length,hash);
+
+
+
+      //printf("The hash is %x \n",hash);
+
+      //printf("HASH:\n");
+      fprintf(fp,"ITERATION START %d INSIDE THREAD: %d \n",i,(char *) msg);
+      fprintf(fp,"PLAINTEXT: %s\n",data);
+      fprintf(fp, "HASH:\n");
+
+      for(x = 0; x < sizeof(hash); x++ )
+      {
+     
+        //printf("%x", (char)hash[x]);
+        fprintf(fp,"%x", (char)hash[x]);
+      }
+      
+      fprintf(fp,"\n");
+      //printf("\n\n");
+      
+      
+       //fprintf(fp, "END ITERATION %ld INSIDE PROCESS TIME: %s \n", i, asctime (timeinfo));
+    
+      //printf("ITERATION %i END \n\n",i); 
+      fprintf(fp,"ITERATION END %d INSIDE THREAD: %d \n\n",i,(char *) msg);
+    }
+
+     //threadTime = time(NULL);
+     threadEndTime = time(NULL);
+      printf("** %s END** TIME: %s  \n\n",(char*) msg,asctime( localtime(&threadEndTime) ) );
+      fprintf(pFile," %s END TIME: %s  \n\n",(char*) msg,asctime( localtime(&threadEndTime) ) );
+      fflush(pFile);
+  
+    //fclose(fp);
+
+
+    pthread_mutex_unlock (&printfLock);
+  
+   return NULL;
+}
+
